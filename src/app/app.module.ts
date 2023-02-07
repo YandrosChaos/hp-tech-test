2ed737de4a2ce165124273ef95450d226cef21d7
CROM <victor.develops@gmail.com>
2023-02-07 19:06:26 +0100

    <> autocommit.sh <>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DatabaseService } from "@services/database/database.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [DatabaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { User } from "@interfaces/user.interface";
+import { DatabaseService } from "@services/database/database.service";
+import { liveQuery, Observable } from "dexie";
 
 @Component({
   selector: "app-login",
@@ -7,10 +10,22 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly databaseService: DatabaseService
+  ) {}
 
-  public onSignIn(): void {
-    this.router.navigate(["/cities"]);
+  public async onSignIn(): Promise<void> {
+    await this.databaseService.updateUser({
+      username: "root",
+      password: "root",
+    });
+    await this.databaseService
+      .userExists({ username: "root", password: "root" })
+      .then((response: boolean) => {
+        if (response) this.router.navigate(["/cities"]);
+        else alert("Wrong username or password");
+      });
   }
 
   public onSignUp(): void {}
