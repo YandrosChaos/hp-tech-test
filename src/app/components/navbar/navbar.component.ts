562d4a2f8c8f4e706fe99d96611c15ff129f5b6c
CROM <victor.develops@gmail.com>
2023-02-08 19:04:00 +0100

    logout updated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Router } from "@angular/router";
+import { UserService } from "@services/user/user.service";
 
 @Component({
   selector: "app-navbar",
@@ -9,13 +10,17 @@ import { Router } from "@angular/router";
 export class NabvarComponent {
   @Output() public searching: EventEmitter<string> = new EventEmitter();
 
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {}
 
   public onSearch(): void {
     this.searching.emit();
   }
 
   public onLogout(): void {
+    this.userService.sessionUser = undefined;
     this.router.navigate(["/login"]);
   }
 }
